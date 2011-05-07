9776506e8cc6619661923c4f05f4748b58fea2a2
Ysiad Ferreiras <ysiadf@gmail.com>
2011-05-07 11:37:43 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Allincomefoods::Application.config.secret_token = '8ceb9ff1d3753ca9416d0b1764279d56ab82a239b11a665dda34053abf4c272fbc89a4f31828517e2a4e7d3c59b1761d6cd97ded64fe4a73eac37e358e696da1'
