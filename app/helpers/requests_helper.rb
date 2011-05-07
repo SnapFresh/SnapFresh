7023f415720222537c739829f471e5adf5abf824
Ysiad Ferreiras <ysiadf@gmail.com>
2011-05-07 16:56:52 -0700

    dynamic zip query

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -23,9 +23,10 @@ module RequestsHelper
             :zip => result["geometry"]["bounds"]  
           }
         end
-        zip = address[-5,4]
+        zip = address[-5,4]+"%"
         #SELECT city, street, name, AVG(3956 * 2 * ASIN(SQRT(POWER(SIN((37.4404 - abs(`lat`)) * pi()/180 / 2),2) + COS(37.4404 * pi()/180 ) * COS(abs(`lat`) * pi()/180) * POWER(SIN((-121.8705 - `long`) * pi()/180 / 2), 2) ))) AS distance
-        @retailer = Retailer.find_by_sql("SELECT name, street, city, state, zip FROM retailers WHERE zip like '9410%'")      
+        #@retailer = Retailer.find_by_sql("SELECT name, street, city, state, zip FROM retailers WHERE zip like '9410%'")      
+        @retailer = Retailer.find_by_sql("SELECT name, street, city, state, zip FROM retailers WHERE zip like \"#{zip}\"")      
         #return parse
         return @retailer.to_s
         #return zip
