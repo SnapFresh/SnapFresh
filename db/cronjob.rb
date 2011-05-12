d21d2e0d75542d9fc17f949bbc51f4d1343311b5
jerseycoder <michelle@codeforamerica.org>
2011-05-11 18:39:34 -0700

    add .zip extract to script

--- db/cronjob.rb
+++ db/cronjob.rb
@@ -1,5 +1,14 @@
 #!/usr/bin/env ruby
 require 'net/http'
+require 'rubygems'
+require 'zip/zip'
+  
+def unzip_file (file)
+  Zip::ZipFile.open(file) { |zip_file|
+   zip_file.each { |f|
+    zip_file.extract(f) 
+   }
+  }
 
 Net::HTTP.start("www.snapretailerlocator.com") { |http|
   states = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN",
@@ -7,12 +16,16 @@ Net::HTTP.start("www.snapretailerlocator.com") { |http|
             "WI", "WV", "WY"]
   $i = 0;
   $num = 50;
+  # Pull in all the state ZIP files containing the CSV files
   while $i < $num  do
     resp = http.get("/export/" + states[$i] + ".zip")
     open(states[$i] + ".zip", "wb") { |file|
     file.write(resp.body)
+    unzip_file(states[$i] + ".zip")
     }
     $i +=1;
-  end 
+  end
+  # Unzip all of the ZIP files
+end
 }
-puts "Done Importing!!"
+puts "Done Importing & Unzipping!"
