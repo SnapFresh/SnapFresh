9cef08ddf70f5208e5bfee990078dbf80a0412f0
jerseycoder <michelle@codeforamerica.org>
2011-05-11 21:27:39 -0700

    Ruby file to import State Zips and concatenate into one master CSV file for all US

--- db/cronjob.rb
+++ db/cronjob.rb
@@ -1,31 +1,28 @@
 #!/usr/bin/env ruby
 require 'net/http'
-require 'rubygems'
-require 'zip/zip'
-  
-def unzip_file (file)
-  Zip::ZipFile.open(file) { |zip_file|
-   zip_file.each { |f|
-    zip_file.extract(f) 
-   }
-  }
 
-Net::HTTP.start("www.snapretailerlocator.com") { |http|
+Net::HTTP.start("www.snapretailerlocator.com") do |http|
   states = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN",
             "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA",
             "WI", "WV", "WY"]
-  $i = 0;
-  $num = 50;
+
   # Pull in all the state ZIP files containing the CSV files
-  while $i < $num  do
-    resp = http.get("/export/" + states[$i] + ".zip")
-    open(states[$i] + ".zip", "wb") { |file|
-    file.write(resp.body)
-    unzip_file(states[$i] + ".zip")
-    }
-    $i +=1;
+  states.each  do |state|
+    resp = http.get("/export/" + state + ".zip")
+    open(state + ".zip", "wb") do |file|
+      file.write(resp.body)   
+    end
+    system("unzip -o #{state}.zip")
   end
   # Unzip all of the ZIP files
 end
-}
 puts "Done Importing & Unzipping!"
+
+File.open("all.csv","w") do |outfile|
+  (Dir.glob("*.csv") - ["all.csv"]).each do |csv_file|
+    File.open(csv_file) do |infile|
+      infile.readline #skip the first line
+      outfile << infile.read
+    end
+  end
+end
