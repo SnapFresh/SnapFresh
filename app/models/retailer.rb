3ab11872ffec4c44038c50316e54638b37d522dc
jerseycoder <michelle@codeforamerica.org>
2011-05-15 17:35:15 -0700

    Added yelp based business type to nearaddy controller function

--- app/models/retailer.rb
+++ app/models/retailer.rb
@@ -1,5 +1,39 @@
 class Retailer < ActiveRecord::Base
     acts_as_mappable :lng_column_name => :lon
+    
+    def retailer_types
+      # Use Yelp API to figure out what kind of establishment each returned retailer is
+      yelpurl = "http://api.yelp.com/business_review_search?term="
+      re1='((?:[a-z][a-z]+))'	# Word 1
+      re2='(\\s+)'	# White Space 1
+      re3='(\\d+)'	# Integer Number 1
+      re=(re1+re2+re3)
+      m=Regexp.new(re,Regexp::IGNORECASE);
+      escfir = self.name
+      if m.match(self.name)
+        escfir=m.match(self.name)[1];
+        #puts "("<<word1<<")"<< "\n"
+      end
+      escname =  URI.escape(escfir, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+      esclocation = "&location=" + URI.escape(self.street, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")) + "%2C" +
+        URI.escape(self.city, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")) + "%2C" + URI.escape(self.state, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+      request = yelpurl + escname + esclocation + "&ywsid=VR0eNW8-767FtIrg21dKAA"
+      puts request
+      #http://api.yelp.com/business_review_search?term=cream%20puffs&location=650%20Mission%20St%2ASan%20Francisco%2A%20CA&ywsid=XXXXXXXXXXXXXXXX
+      url = URI.parse(request)
+      yelpdata = Net::HTTP.get_response(url)    
+      # yelpdata.body is a json file
+      # Now parse the JSON for the retailer type - i.e. "Categories"
+      data = yelpdata.body
+      # we convert the returned JSON data to native Ruby
+      # data structure - a hash
+      result = JSON.parse(data)
+      # if the hash has 'Error' as a key, we raise an error
+       if result.has_key? 'Error'
+        raise "web service error"
+       end
+      return result["businesses"][0]["categories"].map {|c| c["category_filter"]}
+    end
 
     def self.search(search)
       if search
