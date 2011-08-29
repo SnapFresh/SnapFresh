c2098a79d642d81af9f5ebaebb48c07dde37445c
Michelle Koeth <michelle@codeforamerica.org>
2011-08-28 17:50:35 -0700

    Use or statement instead of if-then to avoid a default not being set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,9 @@ class ApplicationController < ActionController::Base
   #  I18n.locale = params[:locale] || I18n.default_locale
   #end
   def set_locale
-    if params[:locale] then
-      I18n.locale = params[:locale] || I18n.default_locale
-    else
-      logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
-      I18n.locale = extract_locale_from_accept_language_header
-      logger.debug "* Locale set to '#{I18n.locale}'"
-    end
+      I18n.locale = params[:locale] || extract_locale_from_accept_language_header || I18n.default_locale
+      # logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+      # logger.debug "* Locale set to '#{I18n.locale}'"
   end
   def default_url_options(options={})
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
