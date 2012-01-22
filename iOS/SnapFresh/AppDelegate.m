d05c07f882498535373ff4086b2776afeadc59fb
shrtlist <shrtlist@gmail.com>
2012-01-22 05:20:25 -0800

    Use appearance proxy to modify the tint color for all instances of UINavigationBar.

--- iOS/SnapFresh/AppDelegate.m
+++ iOS/SnapFresh/AppDelegate.m
@@ -28,6 +28,9 @@
     UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
 
     splitViewController.delegate = (id)navigationController.topViewController;
+    
+    UIColor *color = [UIColor colorWithRed:0.39 green:0.60 blue:0.2 alpha:1.0];
+    [[UINavigationBar appearance] setTintColor:color];
 
     return YES;
 }
