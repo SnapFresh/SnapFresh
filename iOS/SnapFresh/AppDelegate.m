bb70fffac98d5aed28669f6d7ae787b775280a59
shrtlist <shrtlist@gmail.com>
2013-06-13 18:34:34 -0700

    Conditionally set tintColor of navigation bar

--- iOS/SnapFresh/AppDelegate.m
+++ iOS/SnapFresh/AppDelegate.m
@@ -38,6 +38,8 @@
     [RKClient clientWithBaseURLString:kSnapFreshBaseURL];
     [[RKClient sharedClient] setTimeoutInterval:kSnapFreshTimeout];
 
+    UIColor *color = [UIColor colorWithRed:0.39 green:0.60 blue:0.2 alpha:1.0];
+
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
     {
         UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
@@ -46,9 +48,11 @@
 
         splitViewController.delegate = (id)navigationController.topViewController;
     }
+    else
+    {
+        [[UINavigationBar appearance] setTintColor:color];
+    }
     
-    UIColor *color = [UIColor colorWithRed:0.39 green:0.60 blue:0.2 alpha:1.0];
-    [[UINavigationBar appearance] setTintColor:color];
     [[UIToolbar appearance] setTintColor:color];
     [[UISearchBar appearance] setTintColor:color];
 
