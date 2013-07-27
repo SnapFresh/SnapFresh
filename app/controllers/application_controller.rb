a7e77f59855b35a64e6e86070001afa3332fdac4
Jason Wieringa <jason@codesherpas.com>
2013-07-27 14:57:25 -0400

    whitespace and debug statement cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :set_locale
-  #def set_locale
-  #  I18n.locale = params[:locale] || I18n.default_locale
-  #end
+
   def set_locale
-      I18n.locale = params[:locale] || extract_locale_from_accept_language_header || I18n.default_locale
-      # logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
-      # logger.debug "* Locale set to '#{I18n.locale}'"
+    I18n.locale = params[:locale] || extract_locale_from_accept_language_header || I18n.default_locale
   end
+
   def default_url_options(options={})
-    logger.debug "default_url_options is passed options: #{options.inspect}\n"
     { :locale => I18n.locale }
   end
-  
-  private
+
+private
+
   def extract_locale_from_accept_language_header
     request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
+
 end
