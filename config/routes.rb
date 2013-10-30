9a02a53118c85e8b9cbe08743dac938892769d49
Jason Wieringa <jason@jasonwieringa.com>
2013-10-30 16:28:34 -0400

    Remove unused views and routes
    To the best of my ability, I've verfied that everything removed
    here isn't linked to or used as an end point.

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,9 @@
 Allincomefoods::Application.routes.draw do
-  #scope "(:locale)", :locale => /en|es/ do
-  #  resources :retailers
-  #end
   root :to => 'retailers#index'
 
-  match 'browse' => 'retailers#browse'
-  match 'retailers/terms', :to=>'retailers#terms'
-  #resources :requests
-  match 'requests/:address' => 'requests#retailers'
-  match 'aboutus' => 'retailers#aboutus'
-  get "lookup/new"
-  #match 'retailers/near/:lat/:lon' => 'retailers#near',
-  #                      :constraints => { :lat => /[-]?[0-9]+\.?[0-9]*/,
-  #                                        :lon => /[-]?[0-9]+\.?[0-9]*/ }
-  #match 'retailers/list/:address' => 'retailers#list'
+  get 'about' => 'pages#about'
+  get 'terms' => 'pages#terms'
 
-  #match 'retailers/nearaddy(.:format)' => 'retailers#nearaddy'
   resources :retailers do
     collection do
       get "nearaddy"
--- test/functional/retailers_controller_test.rb
+++ test/functional/retailers_controller_test.rb
@@ -6,6 +6,20 @@ class RetailersControllerTest < ActionController::TestCase
     @retailer = retailers(:one)
   end
 
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get nearaddy" do
+    get :nearaddy, :address => "22314"
+    assert_response :success
+    assert_not_nil assigns(:retailers)
+    # TODO-jw This assertion should be specific
+    assert_equal assigns(:retailers).count, 2
+  end
+
+### These are general controller tests that need a better home ###
   test "should set translation by locale" do
     %w(en es zh).each do |language|
       get :index, :locale => language
@@ -29,59 +43,6 @@ class RetailersControllerTest < ActionController::TestCase
     assert_response :success
     assert locale = "en"
   end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
-
-  test "should get aboutus" do
-    get :aboutus
-    assert_response :success
-  end
-
-  test "should get terms" do
-    get :terms
-    assert_response :success
-    assert_not_nil assigns(:terms)
-  end
-
-  test "should get browse" do
-    get :browse
-    assert_response :success
-    # TODO test that the correct list of retailers is being returned
-    assert_not_nil assigns(:retailers)
-  end
-
-  test "should get nearaddy" do
-    get :nearaddy, :address => "22314"
-    assert_response :success
-    # TODO This assertion should be specific
-    # assert_equal Retailer.all, assigns(:retailers)
-    assert_not_nil assigns(:retailers)
-  end
-
-  # TODO is :show being used for anything?
-  # test "should show retailer" do
-  #   get :show, :id => @retailer.id
-  #   assert_response :success
-  #   assert_equal retailers(:one), assigns(:retailer)
-  # end
-
-  # TODO Is :list being used by anyting? It's not being used by rails
-  # Perhaps the iOS app?
-  # test "should get list" do
-  #   get :list
-  #   # TODO These tests should assert there is correct lat/long
-  #   assert_not_nil assigns(:latlon)
-  #   assert_not_nil assigns(:lat)
-  #   assert_not_nil assigns(:long)
-  #   # TODO this should be passing lat/long from params
-  #   assert_redirected_to near_retailers_path
-  # end
-
-  # TODO How is this being used?
-  # test "should get neargeo" do
-  # end
+##################################################################
 
 end
