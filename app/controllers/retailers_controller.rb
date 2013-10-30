9a7f7a5335091b2b8ecafadcc0f67f7462ad2560
Jason Wieringa <jason@jasonwieringa.com>
2013-10-30 16:28:34 -0400

    Remove unused views and routes
    To the best of my ability, I've verfied that everything removed
    here isn't linked to or used as an end point.

--- app/controllers/retailers_controller.rb
+++ app/controllers/retailers_controller.rb
@@ -1,63 +1,7 @@
 class RetailersController < ApplicationController
-  # GET /retailers
-  # GET /retailers.xml
-  helper_method :sort_column, :sort_direction
 
+  # Root
   def index
-
-  end
-
-  def terms
-    @terms = "Our Policies: 1) Parties - these terms are defined between yourself (the User) and the SnapFresh product team (Product Team)
-    which includes volunteer developers, marketers and testers of this application; 2) Privacy - We value your privacy.
-    We will not give, share, sell, rent or transfer any personal information to anyone outside of the Product Team, unless we have your
-    consent. We may track usage of the service, so that the service can improve to suit the User's needs, but we will not
-    share usage data outside of the Product Team; 3) Disclaimer - the information and listings shared on SnapFresh may not be
-    correct, and occasionally SnapFresh may not be available. Therefore, your use of SnapFresh and any reliance upon the
-    information shared on SnapFresh is at your own risk."
-    respond_to do |format|
-      format.html
-      format.xml  { render :xml => @terms }
-      format.json { render :json => @terms }
-      format.text { render :text => @terms}
-    end
-  end
-
-  def aboutus
-
-  end
-
-  def browse
-    @retailers = Retailer.search(params[:search]).order( sort_column + " " + sort_direction).paginate(:page => params[:page])
-
-    #respond_to do |format|
-    #  format.html # index.html.erb
-    #  format.xml  { render :xml => @retailers }
-    #end
-  end
-
-  def list
-    @latlon = get_geo_from_google(params[:address])
-    @lat = @latlon[:lat]
-    @lon = @latlon[:lon]
-
-    redirect_to :controller => 'retailers', :action => 'near', :params =>
-                                                                params[ :lat => @lat, :lon => @lon ]
-  end
-
-  # GET /retailers/near/:lat/:lon
-  def near_geo
-    origin = [params[:lat],params[:lon]]
-    @retailers = Retailer.find :all,
-                              :origin => origin,
-                              #:within => 5,
-                              :order => 'distance',
-                              :limit => 5
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @retailer }
-      format.json { render :json => @retailer }
-    end
   end
 
   # GET /retailers/nearaddy/:address
@@ -81,6 +25,8 @@ class RetailersController < ApplicationController
       format.text { render :text => @retailers.to_enum(:each_with_index).map{|r, i| r.name = "#{i+1} (#{@rt[i][:dist]} #{@rt[i][:unit]}): #{r.name}\n#{r.text_address}"}.join("\n\n")}
     end
   end
+
+  private
 
   def get_geo_from_google(address)
    geocoder = "http://maps.googleapis.com/maps/api/geocode/json?address="
@@ -109,26 +55,4 @@ class RetailersController < ApplicationController
 
     return parse
   end
-
-  # GET /retailers/1
-  # GET /retailers/1.xml
-  def show
-    @retailer = Retailer.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @retailer }
-    end
-  end
-
-  private
-    def sort_column
-      Retailer.column_names.include?(params[:sort]) ? params[:sort] : "name"
-    end
-
-    def sort_direction
-      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
-    end
-
 end
-
