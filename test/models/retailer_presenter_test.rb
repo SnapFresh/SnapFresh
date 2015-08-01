8ec804c98a208a99305675dad8bb20f34893181b
Tom Allen <tommarkallen@gmail.com>
2015-08-01 13:43:33 -0700

    Added tests to retailer_presenter_test.rb

--- test/models/retailer_presenter_test.rb
+++ test/models/retailer_presenter_test.rb
@@ -3,13 +3,21 @@ require 'test_helper'
 describe RetailerPresenter do
 
   before do
+    @address = "22314"
     VCR.use_cassette('retailer-presenter-before') do
-      @retailer_presenter = RetailerPresenter.new("22314")
+      @retailer_presenter = RetailerPresenter.new(@address)
     end
   end
 
   test "should return retailers" do
     assert_equal @retailer_presenter.retailers, [retailers(:one), retailers(:two)]
+  end
+
+
+  test "GoogleGeocoder should return correct lat and long" do
+    VCR.use_cassette('retailer-presenter-google') do
+      assert_equal @retailer_presenter.send(:retrieve_lat_long, @address), [38.7999723, -77.0506896]
+    end
   end
 
   test "should return lat and long" do
@@ -18,6 +26,14 @@ describe RetailerPresenter do
 
   test "retailer should have a distance" do
     assert_equal @retailer_presenter.retailers.map(&:distance), [{:dist=>452.98, :unit=>"mi"}, {:dist=>5627.86, :unit=>"mi"}]
+  end
+
+  test "should convert retailer to txt message format" do
+    expect_text = ["1 (452.98 mi): CIRCLE K STORE 8103",
+                  "3603 Maybank Hwy Johns Island SC\n",
+                  "2 (5627.86 mi): Two Local Mart",
+                  "Two Street Two Town VA"].join("\n")
+    assert_equal @retailer_presenter.to_text, expect_text
   end
 
 end
