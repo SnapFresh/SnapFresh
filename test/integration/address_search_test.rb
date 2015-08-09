b62fadeb83e3a01fd08ac836c00cfd536078dc7d
Jason Wieringa <jason@jasonwieringa.com>
2015-08-09 13:31:44 -0400

    Fix capybara integration tests

--- test/integration/address_search_test.rb
+++ test/integration/address_search_test.rb
@@ -7,18 +7,18 @@ class AddressSearchTest < ActionDispatch::IntegrationTest
   test "should return retailers by zipcode" do
     VCR.use_cassette('address-search-test-by-zipcode') do
       visit '/'
-      fill_in('addressfield', :with => '22314')
-      find('#submitbutton').click
-      assert_selector('ul li', :count => 2)
+      fill_in('address', with: '22314')
+      find('.submit').click
+      assert_selector('main ul li', count: 2)
     end
   end
 
   test "should return retailers by street address" do
     VCR.use_cassette('address-search-by-street-address') do
       visit '/'
-      fill_in('addressfield', :with => '601 Jefferson Street, Alexandria, VA')
-      find('#submitbutton').click
-      assert_selector('ul li', :count => 2)
+      fill_in('address', with: '601 Jefferson Street, Alexandria, VA')
+      find('.submit').click
+      assert_selector('main ul li', count: 2)
     end
   end
 
