2d2321b33ee4cf8beb7cbdf38317df6924aa3794
Marco Abundo <v-maabundo@expedia.com>
2015-11-11 11:04:43 -0800

    Added FarmersMarket unit test

--- iOS/SnapFreshTests/SnapFreshTests.m
+++ iOS/SnapFreshTests/SnapFreshTests.m
@@ -7,6 +7,7 @@
 //
 
 @import XCTest;
+#import "FarmersMarket.h"
 #import "SnapRetailer.h"
 
 @interface SnapFreshTests : XCTestCase
@@ -42,6 +43,21 @@
     XCTAssertEqualObjects(retailer.locality, @"San Francisco", @"Expected equal locality");
     XCTAssertNotNil(retailer.location, @"Expected non-nil location");
     XCTAssertEqualObjects(retailer.postalCode, @"94111", @"Expected equal postalCode");
+}
+
+- (void)testFarmersMarket
+{
+    NSDictionary *farmersMarketDictionary = @{@"marketdetails":@{@"Address":@"111 West St, San Francisco, CA, 94103",
+        @"GoogleLink":@"http://maps.google.com/?q=37.786250%2C%20-122.404883%20(%22Yerba+Buena+Lane+Farmers'+Market%22)",
+        @"Products":@"Baked goods; Cheese and/or dairy products; Eggs; Fresh fruit and vegetables; Fresh and/or dried herbs; Honey; Canned or preserved fruits, vegetables, jams, jellies, preserves, salsas, pickles, dried fruit, etc.; Nuts; Prepared foods (for immediate consumption)",
+        @"Schedule":@"May to October Tue:1:00 PM - 6:00 PM;<br> <br> <br> "}};
+    
+    FarmersMarket *farmersMarket = [[FarmersMarket alloc] initWithDictionary:farmersMarketDictionary];
+    XCTAssertEqualObjects(farmersMarket.administrativeArea, @"CA", @"Expected equal administrativeArea");
+    XCTAssertEqualObjects(farmersMarket.thoroughfare, @"111 West St", @"Expected equal thoroughfare");
+    XCTAssertEqualObjects(farmersMarket.locality, @"San Francisco", @"Expected equal locality");
+    XCTAssertNotNil(farmersMarket.location, @"Expected non-nil location");
+    XCTAssertEqualObjects(farmersMarket.postalCode, @"94103", @"Expected equal postalCode");
 }
 
 @end
